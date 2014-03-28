701d591bdb2cc06e8c2da11bee42b4e0244e4295
nicolas-dular <hello@nicolasdular.com>
2014-03-28 15:26:48 +0100

    blog list

--- functions.php
+++ functions.php
@@ -12,17 +12,3 @@ function get_page_number() {
         print ' • page ' . get_query_var('paged');
     }
 } // end get_page_number
-
-function the_post_widescreen_image() {
-  $post_thumbnail_id = get_post_thumbnail_id( get_the_ID() );
-  $size = apply_filters( 'post_thumbnail_size', 'widescreen' );
-  if ( $post_thumbnail_id ) {
-    do_action( 'begin_fetch_post_thumbnail_html', $post_id, $post_thumbnail_id, $size ); // for "Just In Time" filtering of all of wp_get_attachment_image()'s filters
-    if ( in_the_loop() )
-      update_post_thumbnail_cache();
-    $src = wp_get_attachment_image_src( $post_thumbnail_id, $size );
-  } else {
-    $src = array('');
-  }
-  return $src[0];
-}
--- multiple.php
+++ multiple.php
@@ -9,20 +9,18 @@
   <div class="row">
     <div class="col-lg-8 col-lg-offset-2">
       <?php while ( have_posts() ) : the_post(); ?>
-
-        <article id="article-<?php the_ID(); ?>" class="article" >
+        <article  class="article" >
           <div class="article-header">
             <h1 class="article-title">
               <a href="<?php the_permalink(); ?>" title="<?php printf('Permalink to %s', the_title_attribute('echo=0') ); ?>" rel="bookmark">
                 <?php the_title(); ?>
               </a>
+              <br><small><?php the_time( get_option( 'date_format' ) ); ?></small>
             </h1>
 
           </div>
 
           <div class="content">
-            <img src="<?php echo the_post_widescreen_image(); ?>" class="featured-image" />
-            <?php the_post_widescreen_image(); ?>
             <?php the_excerpt(); ?>
 
             <a href="<?php the_permalink(); ?>" class="btn btn-primary" title="<?php printf( __('Permalink to %s', 'saloon_io'), the_title_attribute('echo=0') ); ?>" rel="bookmark">
