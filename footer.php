e9bb3177bf77208adc3fe6377862b4ed321a5bc4
nicolas-dular <hello@nicolasdular.com>
2014-03-28 12:55:54 +0100

    added about author, footer, and mailchimp subscribe

--- footer.php
+++ footer.php
@@ -1,16 +1,3 @@
-<div class="footer">
-  <div class="container">
-    <div class="row">
-      <div class="col-lg-8 col-lg-offset-2">
-        <h3>Who are we?</h3>
-        <p>We're just two developers who love creating products. On this site we want to share some of our projects, ideas and experiences with you.</p>
-        <p>If you want to <strong>stay up to date</strong>, join 10003042383 Other People and subscribe here:</p>
-      </div>
-    </div>
-  </div>
-</div>
-
-
   <script>
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
--- single.php
+++ single.php
@@ -32,14 +32,38 @@
       <a href="#content"><i class="fa fa-angle-down blog-single-scroll-down"></i></a>
     </div>
   </div>
+
   <div class="container" id="content">
-      <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-        <div class="content">
-          <?php the_content(); ?>
-          <?php wp_link_pages('before=<div class="page-link">Pages:&after=</div>') ?>
+    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+      <div class="content">
+        <?php the_content(); ?>
+        <?php wp_link_pages('before=<div class="page-link">Pages:&after=</div>') ?>
+      </div>
+    </article>
+
+    <div class="about-author">
+      <hr>
+      <div class="media">
+        <img src="https://secure.gravatar.com/avatar/<?php echo md5(get_the_author_meta('email'))?>?s=200" class="img-circle pull-left media-object">
+        <div class="media-body">
+          <h3 class="media-heading">About <?php the_author_meta('first_name'); ?></h3>
+          <p>
+            <?php the_author_meta('description'); ?>
+          </p>
         </div>
-      </article>
+      </div>
+    </div>
   </div>
+
+
 <?php endwhile; ?>
+
+
+<div class="subscribe">
+  <h3>Who are we?</h3>
+  <p>We're just two developers who love creating products. On this site we want to share some of our projects, ideas and experiences with you.</p>
+  <p>If you want to <strong>stay up to date</strong>, join <strong>1 billion subscribers</strong>:</p>
+  <?php mailchimpSF_signup_form(); ?>
+</div>
 
 <?php get_footer(); ?>
