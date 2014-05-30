f6ff032c182b376eb1b23118cde3431cf93bfc9f
nicolas-dular <hello@nicolasdular.com>
2014-05-30 13:18:54 +0200

    blog-list design

--- multiple.php
+++ multiple.php
@@ -7,34 +7,24 @@
 
 <div class="container blog-list">
   <div class="row">
-    <div class="col-lg-8 col-lg-offset-2">
-      <?php while ( have_posts() ) : the_post(); ?>
-        <article  class="article" >
-          <div class="article-header">
-            <h1 class="article-title">
-              <a href="<?php the_permalink(); ?>" title="<?php printf('Permalink to %s', the_title_attribute('echo=0') ); ?>" rel="bookmark">
+    <?php while ( have_posts() ) : the_post(); ?>
+      <div class="col-xs-12 col-sm-6 col-md-4">
+          <a href="<?php the_permalink(); ?>" title="<?php printf('Permalink to %s', the_title_attribute('echo=0') ); ?>" class="article" >
+            <div class="article-header">
+              <div class="article-header-thumbnail">
+                <?php $imgsrc = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), "large"); ?>
+                <img src="<?=$imgsrc[0]?>">
+              </div>
+              <h1 class="article-title">
                 <?php the_title(); ?>
-              </a>
-              <br><small><?php the_time( get_option( 'date_format' ) ); ?></small>
-            </h1>
-
-          </div>
-
-          <div class="content">
-            <?php the_excerpt(); ?>
-
-            <a href="<?php the_permalink(); ?>" class="btn btn-primary" title="<?php printf( __('Permalink to %s', 'saloon_io'), the_title_attribute('echo=0') ); ?>" rel="bookmark">
-              <?php _e( 'Read more', 'saloon_io' ); ?>
-            </a>
-          </div>
-
-          <div class="share">
-            <div style="clear:both"></div>
-          </div>
-
-        </article>
-
-      <?php endwhile; ?>
-    </div>
+                <br><small><?php the_time( get_option( 'date_format' ) ); ?></small>
+              </h1>
+            </div>
+            <div class="share">
+              <div style="clear:both"></div>
+            </div>
+          </a>
+      </div>
+    <?php endwhile; ?>
   </div>
 </div>
