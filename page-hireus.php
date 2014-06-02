87a9b13a6f2f30044336b0699121cd10376f99ee
nicolas-dular <hello@nicolasdular.com>
2014-06-02 08:14:51 +0200

    first iteration of hire-us page

--- page-hireus.php
+++ page-hireus.php
@@ -7,52 +7,84 @@
 
 <div class="container hire-us">
 
-  <div class="row">
-    <div class="col-sm-8 col-sm-offset-2 hire-us-title text-center">
+  <div class="row section hire-us-title">
+    <div class="col-sm-12 col-md-offset-2  ">
       <h1 class="animated fadeIn">Let us help you <b>building your product</b>
       <br>
       <small>Building products is more than programming</small>
       </h1>
     </div>
-    <div class="col-sm-8 col-sm-offset-2 ">
+  </div>
+
+  <div class="row section ">
+    <div class="col-sm-8 col-md-offset-2">
       <p>Everyone likes thoughtful products that cares about design and user experience. Our mission is it to <b>build products people love</b>. In the last 8 years we are working on web applications - that means, we've already done every mistake and know what is important.</p>
     </div>
   </div>
 
-  <div class="row">
-    <div class="col-sm-8 col-sm-offset-2 ">
-      <h2>How we work</h2><br>
+  <div class="row section">
+
+    <div class="col-sm-8 col-md-6 col-md-offset-2">
 
       <h4>The right features for the first version</h4>
       <p>
         It's important to stay focused and build features your customers want. We'll help you with market research,
         which features should be build and how it should work.
       </p>
-      <br>
+    </div>
+    <div class="col-sm-3 icon-big">
+      <i class="fa fa-cubes"></i>
+    </div>
+  </div>
+
+  <div class="row section ">
+
+
+    <div class="col-sm-8 col-md-6 col-md-offset-2">
       <h4>Getting customers before launching the product</h4>
       <p>Launching your product without existing customers would be boring. We'll create a landing page where you can
         explain your idea and future customers can sign up for the first release.
       </p>
+    </div>
 
-      <br>
+    <div class="col-sm-3 icon-big">
+      <i class="fa fa-child"></i>
+    </div>
+  </div>
+
+  <div class="row section ">
+    <div class="col-sm-8 col-md-6 col-md-offset-2">
       <h4>Building a lovable product</h4>
       <p>With our experience we build a reliable and stable software your customers will feel excited about.
         It's important to us to provide a good looking user interface and user experience.
       </p>
+    </div>
+    <div class="col-sm-3 icon-big">
+      <i class="fa fa-heart"></i>
+    </div>
 
-      <br>
+  </div>
+
+
+  <div class="row section ">
+    <div class="col-sm-8 col-md-6 col-md-offset-2">
       <h4>Analyzing and testing</h4>
       <p>Your customers don't use the product that often? We'll find out why and what should be changed to get them back
       </p>
-
+    </div>
+    <div class="col-sm-3 icon-big">
+      <i class="fa fa-bar-chart-o"></i>
     </div>
   </div>
 
-  <div class="row">
-    <div class="col-sm-8 col-sm-offset-2 ">
+  <div class="row section">
+    <div class="col-sm-8 col-md-6 col-md-offset-2">
       <h2>Contact us</h2>
-      <p>We recommend a personal and noncommittal conversation with us. Just contact us and we'll schedule up a meeting</p>
+      <p>Meet us for a noncommittal conversation. Just contact us and we'll schedule up a meeting</p>
       <a class="btn btn-primary text-center">Contact us</a>
+    </div>
+    <div class="col-sm-3 icon-big">
+      <i class="fa fa-send"></i>
     </div>
   </div>
 
