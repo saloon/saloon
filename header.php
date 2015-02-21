6b4f1a923ec3a6ced4635aa569bdbfe2f3461938
Philipp Spieß <hello@philippspiess.com>
2015-02-21 19:38:06 +0100

    fixes

--- header.php
+++ header.php
@@ -67,7 +67,7 @@
   <link rel="alternate" type="application/rss+xml" href="<?php bloginfo('rss2_url'); ?>" title="<?php printf('%s latest posts', wp_specialchars( get_bloginfo('name'), 1 ) ); ?>">
   <link rel="alternate" type="application/rss+xml" href="<?php bloginfo('comments_rss2_url') ?>" title="<?php printf('%s latest comments', wp_specialchars( get_bloginfo('name'), 1 ) ); ?>">
   <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
-  <!--<link href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">-->
+  <link href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
 </head>
 
 <body>
